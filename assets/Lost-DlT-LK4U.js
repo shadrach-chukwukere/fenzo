e57086226d2954f798e35898e9d7a2a2595bfb01
Shadrach Chukwukere <shadrachchukwukerechinemerem@gmail.com>
2025-09-25 19:12:53 +0100

    fff

---
+++ assets/Lost-DlT-LK4U.js
@@ -0,0 +1,11 @@
+import{c as t,j as e,L as n}from"./index-BV6FT8Uv.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]],a=t("circle-alert",s);/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]],c=t("house",o);function i(){return e.jsxs("div",{className:"min-h-screen flex flex-col items-center justify-center bg-white px-4 text-center",children:[e.jsx(a,{className:"w-20 h-20 text-orange-500 mb-4"}),e.jsx("h1",{className:"text-6xl font-bold text-orange-500 mb-4",children:"404"}),e.jsx("h2",{className:"text-2xl md:text-3xl font-semibold mb-2 text-gray-800",children:"Oops! Page not found."}),e.jsx("p",{className:"text-gray-600 mb-6",children:"We can’t find the page you’re looking for. It might have been removed or the URL is incorrect."}),e.jsxs(n,{to:"/",className:"inline-flex items-center gap-2 bg-orange-500 text-white px-6 py-3 rounded-md font-medium hover:bg-orange-600 transition",children:[e.jsx(c,{className:"w-5 h-5"}),"Go Back Home"]})]})}export{i as default};
