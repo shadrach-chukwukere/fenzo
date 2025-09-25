442ee4f3424363c84eba8bb67c85c9925611ca90
Shadrach Chukwukere <shadrachchukwukerechinemerem@gmail.com>
2025-09-25 19:29:38 +0100

    sss

--- assets/minus-U8Hx-iro.js
+++
@@ -1,6 +0,0 @@
-import{c as o}from"./index-BV6FT8Uv.js";/**
- * @license lucide-react v0.525.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c=[["path",{d:"M5 12h14",key:"1ays0h"}]],e=o("minus",c);export{e as M};
